9eeab5dd30c88e5680c0912658b82faab5ea0cb7
KelvinC10 <kelvin100404@gmail.com>
2026-04-14 22:14:44 +0800

    added java code

--- app/src/main/java/my/edu/utar/assignmentpart2/MainActivity.java
+++ app/src/main/java/my/edu/utar/assignmentpart2/MainActivity.java
@@ -1,12 +1,17 @@
 package my.edu.utar.assignmentpart2;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,13 +25,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 1. Find the TextView by its ID from your XML
+        TextView tvDate = findViewById(R.id.tvDate);
+
+        // 2. Get the current date from the system
+        Calendar calendar = Calendar.getInstance();
+
+        // 3. Define the format (Example: 14/4/2026)
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/M/yyyy", Locale.getDefault());
+        String currentDate = dateFormat.format(calendar.getTime());
+
+        // 4. Use getString to safely combine the text
+        String dateToShow = getString(R.string.date_format, currentDate);
+        tvDate.setText(dateToShow);
+
+
     }
-    //Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii asdasdadadada
-    //asdsadadadada HIiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
 
-
-
-    // Hi Jie sssssssssssssssssssssshaaaaaaaaaaaaaaannn
-    //Haloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
 
 }
